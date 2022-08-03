982f423b6a67df37d62e2087c7d66a8b879cc55b
Pollapee Sroysuwan <im.oab.ps@gmail.com>
2022-08-03 23:42:54 +1000

    adjust drawing code to use game resolution instead of screen size.

--- src/core/utils.rs
+++ src/core/utils.rs
@@ -1,11 +1,10 @@
 use macroquad::prelude::*;
 
-pub fn draw_text_center(message: &str, font_size: u16, pos_y: f32)
-{
+pub fn draw_text_center(message: &str, font_size: u16, pos_y: f32) {
     let measure = measure_text(message, None, font_size, 1.0);
     draw_text(
         message,
-        (screen_width() - measure.width) / 2.0,
+        (crate::GAME_WIDTH - measure.width) / 2.0,
         pos_y - 10.0 - measure.height,
         font_size as f32,
         BLACK,
--- src/scenes/title.rs
+++ src/scenes/title.rs
@@ -1,74 +1,65 @@
 use crate::core::scene::{GameScene, SceneTransition};
-use macroquad::prelude::*;
 use lerp::Lerp;
-
+use macroquad::prelude::*;
 
 pub struct TitleScene {
-
     hold_down_tick: u128,
-
 }
 
 impl GameScene for TitleScene {
     fn init(&mut self) {}
 
     fn update(&mut self) -> Result<SceneTransition, i32> {
-
-        if is_key_down(KeyCode::Down)
-        {
+        if is_key_down(KeyCode::Down) {
             self.hold_down_tick += crate::ONE_FRAME.as_millis();
             self.hold_down_tick = self.hold_down_tick.min(500);
-        }
-        else
-        {
-            self.hold_down_tick = match self.hold_down_tick.checked_sub(crate::ONE_FRAME.as_millis())
+        } else {
+            self.hold_down_tick = match self
+                .hold_down_tick
+                .checked_sub(crate::ONE_FRAME.as_millis())
             {
                 Some(v) => v,
-                None => 0
+                None => 0,
             };
         }
 
-        if is_key_pressed(KeyCode::Space)
-        {
+        if is_key_pressed(KeyCode::Space) {
             let mut scene = crate::scenes::gameplay::GameplayScene::new();
             scene.init();
             return Ok(SceneTransition::Replace(Box::new(scene)));
         }
-        
 
         Ok(SceneTransition::None)
     }
 
     fn draw(&mut self) {
-        clear_background(WHITE);
+        clear_background(RED);
 
         crate::core::utils::draw_text_center("TITLE", 48, 100.0);
 
-        crate::core::utils::draw_text_center("Press `Space` to start", 24, screen_height() * 0.6);
+        crate::core::utils::draw_text_center(
+            "Press `Space` to start",
+            24,
+            crate::GAME_HEIGHT * 0.6,
+        );
 
         let percentage = self.hold_down_tick as f32 / 500.0;
-        let pos_y = screen_height().lerp( screen_height() * 0.7,percentage);
+        let pos_y = crate::GAME_HEIGHT.lerp(crate::GAME_HEIGHT * 0.7, percentage);
         self.draw_list(pos_y);
     }
 }
 
 impl TitleScene {
     pub fn new() -> Self {
-        Self {
-            hold_down_tick: 0,
-        }
+        Self { hold_down_tick: 0 }
     }
 
     fn draw_list(&self, pos_y: f32) {
-
         crate::core::utils::draw_text_center("hold `down` button", 24, pos_y - 14.0);
 
         crate::core::utils::draw_text_center("Unlocks:", 24, pos_y + 40.0);
         crate::core::utils::draw_text_center("[ ] ending: 1", 24, pos_y + 100.0);
         crate::core::utils::draw_text_center("[ ] ending: 2", 24, pos_y + 140.0);
         crate::core::utils::draw_text_center("[ ] ending: 3", 24, pos_y + 180.0);
-
     }
-
-    
 }
